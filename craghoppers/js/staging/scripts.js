ee03ece310bcdb294d3e12cf18dfa70436e85b04
rimmerz <zrimmer@regatta.com>
2018-08-13 07:34:40 +0000

    CHUS header updates

--- craghoppers/js/staging/scripts.js
+++ craghoppers/js/staging/scripts.js
@@ -16,6 +16,38 @@ requirejs(['jquery'], function( $ ) {
   ////////////////////////////////// 
 
 
+  /* CHUS only script */
+  if (('.cms-homepage-promo-grid-ch-us').length > 0){
+
+    console.log('Hello');
+    document.getElementById('search').placeholder='Search here';
+
+    setTimeout(function(){
+      var basketCheck = setInterval(function(){
+        if (jQuery('.c-mini-cart-btn.action.showcart .c-mini-cart-btn__price br').length > 0){
+          jQuery('.c-mini-cart-btn.action.showcart').addClass('basketEmpty');
+          jQuery('.o-layout__item .o-layout:nth-child(2) .o-layout__item:nth-child(1)').addClass('basketEmpty');
+          jQuery('.o-layout__item .o-layout:nth-child(2) .o-layout__item:nth-child(2)').addClass('basketEmpty');
+        } else if(jQuery('.c-mini-cart-btn.action.showcart').length > 0){
+          jQuery('.c-mini-cart-btn.action.showcart').removeClass('basketEmpty');
+          jQuery('.o-layout__item .o-layout:nth-child(2) .o-layout__item:nth-child(1)').removeClass('basketEmpty');
+          jQuery('.o-layout__item .o-layout:nth-child(2) .o-layout__item:nth-child(2)').removeClass('basketEmpty');
+        }
+      }, 1000);
+    }, 6000);
+
+    if (('.authorization-link a:nth-child(2)').length > 0){
+      
+      setTimeout(function(){
+        console.log('Is it me your looking for?');
+        console.log('I can see it in your eyes');
+        console.log('I can see it in your smile');
+        jQuery('.or').html('/');
+      }, 5000);
+    }
+
+  }
+  /* CHUS only script END */
 
 
 
